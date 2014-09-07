e73d97366bfc2372d025d40405cb5dcac368d39f
Alfredo Di Napoli <alfredo.dinapoli@gmail.com>
2014-09-07 15:16:00 +0100

    Fixed parser for func decl

--- examples/log1.rb
+++ examples/log1.rb
@@ -1,8 +1,26 @@
 require 'logger'
 
+require "baz"
+
 
 
 
 def do_log(what_to_log, what_toLog2  , whatToLog3)
   "hello"
+end
+
+def anotherFunc()
+  10
+  "Hello"
+end
+
+def func2()
+  def nested(f1, f2)
+    10
+  end
+
+  10
+  "Hello"
+
+
 end
